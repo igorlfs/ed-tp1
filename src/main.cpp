63624f64fdaa9f11ba54c3052f33777f2ca4ac16
igorlfs <igorlfs@ufmg.br>
2021-12-09 10:27:46 -0300

    doc(main): document functions
    * generalize append()

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
 #include "escalonador.hpp"
 #include "msgassert.hpp"
 
-void appendOut(string &str) {
-    (str.find('.') == string::npos) ? str += "-out"
-                                    : str.insert(str.find('.'), "-out");
+static const string OUT = "-out";
+
+// @brief Adiciona o marcador de saída ao nome do arquivo
+// @param nome do arquivo, marcador de saída
+// @return nome do arquivo com marcador
+void append(string &str, const string c) {
+    (str.find('.') == string::npos) ? str += c : str.insert(str.find('.'), c);
 }
 
+// @brief Executa o programa principal
+// @param argc, argv
+// @return TODO
 int main(int argc, char *argv[]) {
     erroAssert(argc > 1, "Passe o nome do arquivo de entrada como parâmetro");
     string fileName = argv[1];
@@ -14,7 +21,7 @@ int main(int argc, char *argv[]) {
     inputFile.open(fileName);
     erroAssert(inputFile.is_open(), "Erro ao abrir arquivo de entrada");
 
-    appendOut(fileName);
+    append(fileName, OUT);
     Escalonador E(fileName);
 
     E.readFile(inputFile);
