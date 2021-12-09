2640448a4d998c5f21464093f4aa44b71f462698
igorlfs <igorlfs@ufmg.br>
2021-12-09 10:54:05 -0300

    doc(Site)

--- include/linearlist.hpp
+++ include/linearlist.hpp
@@ -2,16 +2,19 @@
 
 class LinearList {
   public:
+    // Construtores
     LinearList() { this->size = 0; }
 
-    // @brief retorna o tamanho da fila
-    // @return tamanho da fila
+    // Getters
     int getSize() const { return this->size; };
+
+    // Capacidade
 
     // @brief determina se a fila está vazia
     // @return true se vazia; false caso contrário
     bool empty() const { return (this->size == 0); }
 
+    // Modificadores
     virtual void clear() = 0;
 
   protected:
--- include/site.hpp
+++ include/site.hpp
@@ -6,21 +6,36 @@ typedef std::string Host;
 
 class Site {
   public:
+    // Construtores
+    // Construtor default para poder usar como célula
     Site() {}
+    // @brief inicia o host e insere a URL na 1ª posição da lista
+    // @param URL
     Site(const URL &u) {
         this->host = u.getHost();
         this->urls.insertBeg(u);
     }
 
+    // Getters
     Host getHost() const { return this->host; }
     LinkedList *getUrls() { return &this->urls; }
 
+    // Setters
+    // @brief inicia o host e insere a URL na 1ª posição da lista
+    // @param URL
     void setHost(const URL &u) {
+        // Em princípio, eu deveria limpar a lista, mas esse método nunca
+        // É usado para sobrepor uma lista antiga
         this->host = u.getHost();
         this->urls.insertBeg(u);
     };
 
+    // Operadores
+    // @brief imprime o Host do site na saida out
+    // @param out
     void printHost(std::ostream &out) const { out << this->host << '\n'; }
+    // @brief imprime as URLs do site na saída out, em ordem
+    // @param out
     void printUrls(std::ostream &out) const { this->urls.print(out); }
 
   private:
